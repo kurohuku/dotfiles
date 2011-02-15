b5223a1273bd47001b23fdafc03d303ec0b6b44e
kurohuku <kurohuku@phorni.net>
2011-02-14 21:45:28 -0800

    Edited emacs/inits/30_yasnippet.el via GitHub

--- emacs/inits/30_yasnippet.el
+++ emacs/inits/30_yasnippet.el
@@ -1,17 +1,15 @@
 ;;;; yasnippet に関する設定
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet/")
-(require 'yasnippet)
+(setq yas/trigger-key "TAB")
 (require 'yasnippet-config)
-(yas/setup "~/.emacs.d/plugins/yasnippet")
+(yas/setup "~/.emacs.d/plugins/yasnippet/")
 
 (setq yas/buffer-local-condition
       '(or (not (or (string= "font-lock-comment-face"
-			     (get-char-property (point) 'face))
-		    (string= "font-lock-string-face"
-			     (get-char-property (point) 'face))))
-	   '(require-snippet-condition . force-in-comment)))
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets")
+                             (get-char-property (point) 'face))
+                    (string= "font-lock-string-face"
+                             (get-char-property (point) 'face))))
+           '(require-snippet-condition . force-in-comment)))
 
 (global-set-key (kbd "C-x y") 'yas/register-oneshot-snippet)
 (global-set-key (kbd "C-x C-y") 'yas/expand-oneshot-snippet)
