cc098ee6a7b95d8c2a3855004dc8982b08654697
kurohuku <kurohuku@phorni.net>
2011-04-11 00:29:49 +0900

    try-complete-slime-symbol

--- emacs/inits/50_mode-specified-hippie-expand.el
+++ emacs/inits/50_mode-specified-hippie-expand.el
@@ -48,6 +48,50 @@
 
 ;;(provide 'mode-specified-hippie-expand)
 
+;;;; for slime-mode
+(defun try-complete-slime-symbol (old)
+  (unless old
+    (he-init-string (he-lisp-symbol-beg) (point))
+    (unless (he-string-member he-search-string he-tried-table)
+      (setq he-tried-table (cons he-search-string he-tried-table)))
+    (setq he-expand-list
+	  (and (not (equal he-search-string ""))
+	       (sort
+		(case slime-complete-symbol-function
+		  ((slime-simple-complete-symbol)
+		   (get-completions/slime-simple-complete he-search-string))
+		  ((slime-fuzzy-completions)
+		   (get-completions/slime-fuzzy-complete-symbol he-search-string))
+		  ((slime-complete-symbol*)
+		   (get-completions/slime-complete-symbol*))
+		  (t (error "unexpected slime-complete-symbol-function")))
+		'string-lessp))))
+  (while (and he-expand-list
+	      (he-string-member (car he-expand-list) he-tried-table))
+    (setq he-expand-list (cdr he-expand-list)))
+  (if (null he-expand-list)
+      (progn
+	(when old (he-reset-string))
+	nil)
+      (progn
+	(he-substitute-string (car he-expand-list))
+	(setq he-expand-list (cdr he-expand-list))
+	t)))
+
+(defun get-completions/slime-simple-complete-symbol (prefix)
+  (car (slime-simple-completions prefix)))
+
+(defun get-completions/slime-fuzzy-complete-symbol (prefix)
+  (car (slime-fuzzy-completions prefix)))
+  
+
+(defun get-completions/slime-complete-symbol* ()
+  " -> slime-maybe-complete-as-filename , slime-expand-abbreviations-and-complete"
+  (let ((end (move-marker (make-marker) (slime-symbol-end-pos)))
+	(beg (move-marker (make-marker) (slime-symbol-start-pos))))
+    (let ((completions (slime-contextual-completions beg end)))
+      (car completions))))
+
 ;;;; examples
 (set-default-try-functions
  '(try-complete-file-name-partially
@@ -60,12 +104,17 @@
 (dolist (mode
 	  '(emacs-lisp-mode
 	    lisp-mode
-	    common-lisp-mode
 	    lisp-interaction-mode))
   (set-mode-specified-try-functions
    mode
    '(try-complete-lisp-symbol-partially
      try-complete-lisp-symbol)))
+
+(dolist (mode '(slime-mode slime-repl-mode))
+  (set-mode-specified-try-functions
+   mode
+   '(try-complete-slime-symbol)))
+
 
 ;;;; enable
 (enable-mode-specified-hippie-expand)
