f00b47c512f88f86f2ef53691288257a3fb9146f
kurohuku <kurohuku@phorni.net>
2011-08-28 03:13:31 +0900

    add erlang.el

---
+++ emacs/inits/20_erlang-mode.el
@@ -0,0 +1,7 @@
+;;;; Last Updated : 2011/08/28 03:13
+
+(require 'erlang)
+(require 'erlang-start)
+(add-to-list 'exec-path *path:erlang-bin-dir*)
+(setf erlang-root-dir *path:erlang-root-dir*)
+(setf erlang-electric-commands '())
