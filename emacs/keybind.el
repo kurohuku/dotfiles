da6c4842b4d12058d8976233f28ec09a89d6b325
kurohuku <kurohuku@phorni.net>
2016-07-06 23:16:57 +0900

    キーバインドの設定方法をbind-keyに変更, guide-key -> which-key

--- emacs/keybind.el
+++ emacs/keybind.el
@@ -3,107 +3,101 @@
 ;; keybind
 
 (defvar helm-prefix-map (make-sparse-keymap))
-(defvar smartrep-prefix-map (make-sparse-keymap))
 
+(require 'bind-key)
 (require 'smartrep)
+(require 'which-key)
 
 ;; global
-(u:defkeys global-map
-  (kbd "<Return>") 'newline-and-indent
-  (kbd "C-;") 'dabbrev-expand
-  (kbd "C-a") 'u:move-bol+scroll-down
-  (kbd "C-e") 'u:move-eol+scroll-up
-  (kbd "C-h") 'delete-backward-char
-  (kbd "C-o") helm-prefix-map
-  (kbd "C-r") smartrep-prefix-map
-  (kbd "C-t") 'u:other-window-or-split
-  (kbd "C-w") 'u:kill-word-or-active-region
-  (kbd "C-x b") 'helm-mini
-  (kbd "C-z") 'eshell
-  (kbd "M-<Return>") 'newline
-  (kbd "M-?") 'help
-  (kbd "M-w") 'u:copy-word-or-active-region
-  (kbd "M-x") 'helm-M-x
-  (kbd "M-%") 'vr/query-replace
-  (kbd "C-<SPC>") 'u:set-mark-command-or-mark-sexp
-  )
+(bind-key* "C-t" 'u:other-window-or-split)
+
+(bind-key "<Return>" 'newline-and-indent)
+(bind-key* "C-;" 'dabbrev-expand)
+(bind-key* "C-a" 'u:move-bol+scroll-down)
+(bind-key* "C-e" 'u:move-eol+scroll-up)
+(bind-key* "C-h" 'delete-backward-char)
+(bind-key* "C-o" helm-prefix-map)
+(bind-key* "C-t" 'u:other-window-or-split)
+(bind-key* "C-w" 'u:kill-word-or-active-region)
+(bind-key* "C-x b" 'helm-mini)
+(bind-key* "C-z" 'eshell)
+(bind-key* "M-<Return>" 'newline)
+(bind-key* "M-?"  'help)
+(bind-key* "M-w" 'u:copy-word-or-active-region)
+(bind-key* "M-x" 'helm-M-x)
+(bind-key* "M-%" 'vr/query-replace)
+(bind-key* "C-<SPC>" 'u:set-mark-command-or-mark-sexp)
+
+
+
+;; mnemonic keymap
+(defvar mnemonic-prefix-map (make-sparse-keymap))
+(defvar mnemonic-search-map (make-sparse-keymap))
+(defvar mnemonic-buffer-map (make-sparse-keymap))
+(defvar mnemonic-help-map (make-sparse-keymap))
+(defvar mnemonic-window-map (make-sparse-keymap))
+(defvar mnemonic-jump-map (make-sparse-keymap))
+(bind-key* "C-:" mnemonic-prefix-map)
+(bind-key "s" mnemonic-search-map mnemonic-prefix-map)
+(bind-key "b" mnemonic-buffer-map mnemonic-prefix-map)
+(bind-key "h" mnemonic-help-map mnemonic-prefix-map)
+(bind-key "w" mnemonic-window-map mnemonic-prefix-map)
+(bind-key "j" mnemonic-jump-map mnemonic-prefix-map)
+
+(bind-keys :map mnemonic-jump-map
+           ("j" . ace-jump-mode)
+           ("w" . ace-jump-word-mode)
+           ("l" . ace-jump-line-mode)
+           ("g" . goto-line))
 
 ;; M-g
-(u:defkeys goto-map
-  (kbd "d") 'u:goto-dotfiles-dired)
+(bind-keys :map goto-map
+           ("d" . u:goto-dotfiles-dired))
 
 ;; C-x
-(u:defkeys ctl-x-map
-  (kbd "r r") 'mc/mark-all-in-region
-  (kbd "r l") 'vr/mc-mark)
+(bind-keys :map ctl-x-map
+           ("r r" . mc/mark-all-in-region)
+           ("r l" . vr/mc-mark))
 
-;; smartrep
-(smartrep-define-key global-map "C-r"
-  `(("h" . 'backward-char)
-    ("j" . 'next-line)
-    ("k" . 'previous-line)
-    ("l" . 'forward-char)
-    ("n" . 'scroll-up)
-    ("p" . 'scroll-down)
-
-    ("." . 'highlight-symbol-next)
-    ("," . 'highlight-symbol-prev))
-  )
 
 ;; mode-specific (C-c `key')
-(u:defkeys mode-specific-map
-  (kbd "c") 'compile
-  (kbd "n") 'next-error
-  (kbd "w") 'sdic-describe-word
-  (kbd "W") 'sdic-describe-word-at-point
-  (kbd "l") 'org-store-link
-  (kbd "a") 'org-agenda)
+(bind-keys :map mode-specific-map
+           ("c" . compile)
+           ("n" . next-error)
+           ("w" . sdic-describe-word)
+           ("W" . sdic-describe-word-at-point)
+           ("l" . org-store-link)
+           ("a" . org-agenda))
 
-(u:defkeys emacs-lisp-mode-map
-  (kbd "M-.") 'find-function
-  (kbd "C-c C-l") 'load-file)
+(bind-keys :map emacs-lisp-mode-map
+           ("M-." . find-function)
+           ("C-c C-l" . load-file))
 
-(u:defkeys ac-completing-map
-  (kbd "C-n") 'ac-next
-  (kbd "C-p") 'ac-previous)
+(bind-keys :map ac-completing-map
+           ("C-c" . ac-next)
+           ("C-p" . ac-previous))
 
-(u:defkeys helm-prefix-map
-  (kbd "o") 'helm-swoop
-  (kbd "s") 'helm-multi-swoop
-  (kbd "S") 'helm-multi-swoop
-  (kbd "y") 'helm-show-kill-ring
-  (kbd "<SPC>") 'helm-all-mark-rings
-  (kbd "<RET>") 'helm-resume
-  (kbd "i") 'helm-imenu
-  (kbd "r") 'helm-recentf)
+(bind-keys :map helm-prefix-map
+           ("o" . helm-swoop)
+           ("s" . helm-multi-swoop)
+           ("S" . helm-multi-swoop)
+           ("y" . helm-show-kill-ring)
+           ("<SPC>" . helm-all-mark-rings)
+           ("<RET>" . helm-resume)
+           ("i" . helm-imenu)
+           ("r" . helm-recentf))
 
-(u:defkeys helm-map
-  (kbd "C-c n") 'helm-next-source
-  (kbd "C-c p") 'helm-previous-source)
+(bind-keys :map helm-map
+           ("C-c n" . helm-next-source)
+           ("C-c p" . helm-previous-source))
 
 (add-hook 'eshell-mode-hook
           (lambda ()
-            (define-key eshell-mode-map (kbd "M-r") 'helm-eshell-history)))
+            (bind-key "M-r" 'helm-eshell-history eshell-mode-map)))
 
 (with-eval-after-load 'paredit
-  (define-key paredit-mode-map
-    (kbd "C-h") 'paredit-backward-delete))
+  (bind-keys :map paredit-mode-map
+             ("C-h" . paredit-backward-delete)))
 
-(require 'guide-key)
-(setq guide-key/guide-key-sequence
-      '("C-x r" "C-x v" "C-x RET"
-        "C-x C-k" "C-o" "ESC g"))
-(defun guide-key/my-hook-function-for-org-mode ()
-  (guide-key/add-local-guide-key-sequence "C-c")
-  (guide-key/add-local-guide-key-sequence "C-c C-x")
-  (guide-key/add-local-highlight-command-regexp "org-"))
-(add-hook 'org-mode-hook 'guide-key/my-hook-function-for-org-mode)
-
-(defun guide-key/my-hook-function-for-gfm-mode ()
-  (guide-key/add-local-guide-key-sequence "C-c C-c")
-  (guide-key/add-local-guide-key-sequence "C-c C-a")
-  (guide-key/add-local-highlight-command-regexp "markdown-"))
-(add-hook 'gfm-mode-hook 'guide-key/my-hook-function-for-gfm-mode)
-
-(setq guide-key/popup-window-position 'bottom)
-(guide-key-mode 1)
+(which-key-mode)
+(which-key-setup-side-window-right-bottom)
